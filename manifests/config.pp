2983c5028375b50391bcd9efc520fe4a73060ba0
Peter Heiner <p.heiner@gmail.com>
2015-02-17 13:24:41 +0000

    fix c&p typo

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,8 @@
 class tftpserver::config (
-    $options = $ntpserver::options,
-    $listen  = $ntpserver::listen,
-    $user  = $ntpserver::user,
-    $tftpdir  = $ntpserver::tftpdir,
+    $options = $tftpserver::options,
+    $listen  = $tftpserver::listen,
+    $user  = $tftpserver::user,
+    $tftpdir  = $tftpserver::tftpdir,
 ) {
     file { $tftpserver::optionsfile :
         content => template('tftpserver/tftpd-hpa.erb')
