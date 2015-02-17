eaa60cdf2d86c27ce50c7ed0990d7e50fa07cee4
Peter Heiner <p.heiner@gmail.com>
2015-02-17 13:23:26 +0000

    add more options

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,8 @@
 class tftpserver::config (
-
+    $options = $ntpserver::options,
+    $listen  = $ntpserver::listen,
+    $user  = $ntpserver::user,
+    $tftpdir  = $ntpserver::tftpdir,
 ) {
     file { $tftpserver::optionsfile :
         content => template('tftpserver/tftpd-hpa.erb')
