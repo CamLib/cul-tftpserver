366708162ff003e852b9f8b00a55a82d3fb116b1
Peter Heiner <p.heiner@gmail.com>
2015-02-17 12:39:10 +0000

    add options

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,12 @@
 class tftpserver (
-
+    $packages    = [].
+    $version     = 'latest',
+    $tftpdir     = '/var/lib/tftpboot',
+    $optionsfile = '/etc/default/tftpd-hpa',
+    $options     = '--secure -c',
 ) {
-
+    class{'tftpserver::install': } ->
+    class{'tftpserver::config': } ~>
+    class{'tftpserver::service': } ->
+    Class["tftpserver"]
 }
-
-  class{'tftpserver::install': } ->
-  class{'tftpserver::config': } ~>
-  class{'tftpserver::service': } ->
-  Class["tftpserver"]
