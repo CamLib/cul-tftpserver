6e2ca03e7c59592b6ffd9de06b8e1df37d14b1d0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-30 22:41:07 +0800

    Delete manifests directory

--- manifests/service.pp
+++
@@ -1,9 +0,0 @@
-class tftpserver::service {
-  $ensure = $tftpserver::start ? {true => running, default => stopped}
- 
-    service{"tftpd-hpa":
-        ensure  => $ensure,
-        enable  => $tftpserver::enable,
-
-    }
-}
